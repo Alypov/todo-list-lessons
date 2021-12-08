0ed2cb6efb7c3656c3274c3b27087d30718e4771
fes2005 <sergeyfes81@gmail.com>
2021-12-08 18:12:06 +0200

    Co-authored-by: Alypov <Alypov@users.noreply.github.com>

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles.css';
 
 const Button = ({text}) => {
-  return <button className ="btn">{text}</button>;
+  return <button className ="btn">Delete</button>;
 };
 
 export default Button;
