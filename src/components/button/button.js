895cdb25c6b6ff7410a0074d867b67874a7f5e8e
fes2005 <sergeyfes81@gmail.com>
2021-12-21 18:48:25 +0200

    add save functionallity

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -2,8 +2,8 @@ import React from 'react';
 import cn from 'classnames';
 import './styles.css';
 
-const Button = ({text, type}) => {
-  return <button className={cn('btn', type)}>{text}</button>;
+const Button = ({text, type, onClick}) => {
+  return <button onClick={onClick} className={cn('btn', type)}>{text}</button>;
 };
 
 export default Button;
