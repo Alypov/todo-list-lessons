3a4375f1d1a313c86097da0313b09b0fd3eb3014
fes2005 <sergeyfes81@gmail.com>
2021-12-22 18:24:35 +0200

    adding functionallity

--- src/components/todo-item/todo-item.js
+++ src/components/todo-item/todo-item.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import Button from '../button/button';
-import './styles.css';
+import React, { useContext } from "react";
+import Button from "../button/button";
+import "./styles.css";
+import Context from "../context/context";
 
-const TodoItem = ({ todoText }) => {
+const TodoItem = ({ todoText, todo}) => {
+  const { removeTodo } = useContext(Context);
   return (
     <div className="todo-wrapper">
-      <div className='left-section'> 
-
-      <input type="checkbox" className="checkbox" value="true"></input>
-      <div>{todoText}</div>
+      <div className="left-section">
+        <input type="checkbox" className="checkbox" value="true"></input>
+        <div>{todoText}</div>
       </div>
-      <Button text='Delete' type='long'/>
+      <Button text="Delete" onClick={() => removeTodo(todo.id)} type="long"/>
     </div>
   );
 };
