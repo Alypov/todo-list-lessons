a21fc8e38cf77bfd4a664d7ff43d3564b9d8aef0
fes2005 <sergeyfes81@gmail.com>
2021-12-23 19:23:25 +0200

    fix adding crossed out todo functionallity

--- src/components/todo-item/todo-item.js
+++ src/components/todo-item/todo-item.js
@@ -1,16 +1,14 @@
 
 import Button from "../button/button";
 import "./styles.css";
+import cn from 'classnames';
 
-const TodoItem = ({ todoText, todoId, removeTodo, todo}) => {
-const classes = []
-if (todoId.completed) {
-  classes.push('done')
-}
+const TodoItem = ({ todoText, todoId, removeTodo, onClick, type}) => {
+
   return (
     <div className="todo-wrapper">
       <div className="left-section">
-        <input type="checkbox" className={classes.join(' ')} value="true"></input>
+        <input type="checkbox" onClick={onClick} className={cn('done',type )} value="true"></input>
         <div>{todoText}</div>
       </div>
       <Button text="Delete" onClick={() => removeTodo (todoId)} type="long"/>
